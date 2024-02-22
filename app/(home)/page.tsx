d27655db58ccc429d8036c66dd5a070e658f98c1
Matheus-kb <matheuskbitencourt@gmail.com>
2024-02-22 11:03:11 -0300

    feat: add initial welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
 import { Fragment } from "react";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
-    <Fragment> 
-      <Header/>
+    <Fragment>
+      <Header />
+      <div className="px-5 pt-5">
+      <h2 className="font-bold text-xl">Olá, Matheus!</h2>
+      <p className="capitalize text-sm">
+        {format(new Date(), "EEE',' dd 'de' MMMM", { locale: ptBR })}
+      </p>
+      </div>
     </Fragment>
   );
 }
